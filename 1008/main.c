6af44cd36477bff1067d48191d736c848df3e00c
charles-ramos <charles@back4app.com>
2020-03-07 11:16:55 -0300

    Resolução Exercício 1008

---
+++ 1008/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int numerof, horast;
+    float valorh;
+
+    scanf("%d%d%f", &numerof, &horast, &valorh);
+    printf("NUMBER = %d\n", numerof);
+    printf("SALARY = U$ %.2f\n", horast * valorh);
+    return 0;
+}
