b50ffcac595fe8215c9ac4e636359e536a5038eb
charles-ramos <charles@back4app.com>
2020-03-07 11:13:05 -0300

    Resolução Exercício 1015

--- 1015/main.c
+++ 1015/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+
+ double x1, x2, y1, y2, dist;
+
+ scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+
+ dist = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+
+ printf("%.4lf\n", dist);
+ return 0;
 }
